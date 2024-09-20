f525dd3d0381eebaa95e6124278bd5b270ac8a66
ModEraNoah <klauck02@outlook.com>
2024-09-20 18:31:41 +0200

    crafting valid PDF

--- main.c
+++ main.c
@@ -40,36 +40,65 @@ char** craftObjectContent(int objectNumber, char contentString[]) {
 }
 
 Object getPdfRoot(int objectNumber) {
-	/*
-	int charCounter = 1;
-	while (objectNumber/(charCounter * 10)) {
-		charCounter++;
-	}
-
-	char objectBeginning[charCounter + 8];
-	snprintf(objectBeginning, sizeof(objectBeginning), "%i 0 obj\n", objectNumber);
-
-	char *objectEnding = "\nendobj";
-	char *rootCatalog = "<</Type /Catalog /Pages 2 0 R>>";
-
-	int length = strlen(objectBeginning) + strlen(rootCatalog) + strlen(objectEnding);
-	char *content= (char*) malloc((length + 8) * sizeof(char));
-
-	strcat(content, objectBeginning);
-	strcat(content, rootCatalog);
-	strcat(content, objectEnding);
-	*/
 	char **content = craftObjectContent(objectNumber, "<</Type /Catalog /Pages 2 0 R>>");
 
 	Object temp = {objectNumber, *content};
 	return temp;
 }
 
-Object getPagesRoot(int objectNumber) {
-	Object temp = {objectNumber,"<</Type /Pages /Kids [3 0 R] /Count 1>>"};
+Object getPagesRoot(int objectNumber, int count, int kids[]) {
+	char **content = craftObjectContent(objectNumber,"<</Type /Pages /Kids [3 0 R] /Count 1>>");
+
+	Object temp = {objectNumber,*content};
 	return temp;
 }
 
+Object createPageObject(int objectNumber, int parent, int contents) {
+	int additionalLen = 0;
+	int charCounter = 1;
+	while (parent/(charCounter * 10)) {
+		charCounter++;
+	}
+	additionalLen = charCounter;
+	charCounter = 1;
+	while (contents/(charCounter * 10)) {
+		charCounter++;
+	}
+	additionalLen += charCounter;
+	char contentString[44 + additionalLen];
+	snprintf(contentString, 44 + additionalLen, "<</Type /Page /Parent %d 0 R /Contents %d 0 R>>", parent, contents);
+
+	char **content = craftObjectContent(objectNumber, contentString);
+
+	Object temp = {objectNumber,*content};
+	return temp;
+}
+
+Object createTextObject(int objectNumber, char text[]) {
+		
+	int textLen = strlen(text);
+	int streamLength = 48+textLen+1;
+	char stream[streamLength];
+	snprintf(stream, streamLength, "stream\nBT\n/F1 14 Tf\n70 300 TD\n(%s) Tj\nET\nendstream", text);
+
+	int charCounter = 1;
+	while (streamLength/(charCounter * 10)) {
+		charCounter++;
+	}
+	char dict[13+charCounter] ;
+	snprintf(dict, 13+charCounter, "<</Length %d>>\n", streamLength);
+
+	char contentString[streamLength + 13 + charCounter];
+	strcat(contentString,dict);
+	strcat(contentString,stream);
+
+	char **content = craftObjectContent(objectNumber, contentString);
+
+	Object temp = {objectNumber,*content};
+	return temp;
+}
+
+static int currentLength = 10;
 char** getXref(Object obs[]) {
 	char ** t = malloc (1 * sizeof(char*));
 
@@ -78,49 +107,52 @@ char** getXref(Object obs[]) {
 	char temp[6] = "0 ";
 	strcat(temp, num);
 	t[0] = malloc(1000 * sizeof(char));
+	strcat(t[0], "xref\n");
 	strcat(t[0], temp);
 	strcat(t[0],"0000000000 65535 f\n");
-	int currentLength = 10;
 	for (int i = 0; i < objectCounter; i++) {
-		int offset;
-		if (i == 0)
-			offset = currentLength;
-		else 
-			offset = 16 + currentLength;
 		char numb[11];
-		snprintf(numb, sizeof(numb), "%010d", offset);
-		currentLength += offset;
-		currentLength += strlen(obs[i].content);
+		snprintf(numb, sizeof(numb), "%010d", (currentLength));
+		currentLength += (strlen(obs[i].content) + 1);
 		strcat(t[0], numb);
 		strcat(t[0], " 00000 n\n");
-		
-		// currentLength = strlen(t[0]);
 	}
+	strcat(t[0], "trailer\n");
 	return t;
 }
 
 int main(void) {
-	Object obs[2];
+	Object obs[4];
 	obs[0] = getPdfRoot(++objectCounter);
-	obs[1] = getPagesRoot(++objectCounter);
+	obs[1] = getPagesRoot(++objectCounter, 1, (int[]){3});
 	printf("%s",getStart());
 
 	printf("%s\n", obs[0].content);
-	free(obs[0].content);
+	// free(obs[0].content);
 
 	// printf("%i 0 obj\n", obs[1].objectNumber);
 	// printf("%s\n", obs[1].content);
 	// printf("endobj\n");
-	// printf("%s\n", obs[1].content);
+	printf("%s\n", obs[1].content);
 	// free(obs[1].content);
-	//
+
+	int parent = objectCounter;
+	obs[2] = createPageObject(++objectCounter, parent, parent+2);
+	printf("%s\n", obs[2].content);
+
+	obs[3]= createTextObject(++objectCounter, "Hallo Welt!");
+	printf("%s\n", obs[3].content);
+	// free(obs[2].content);
+
 	// // printf("before calling getXref");
-	// char **temp = getXref(obs);
-	// printf("xref\n");
-	// printf("%s", temp[0]);
-	// printf("trailer\n");
-	//
-	// free(temp);
+	char **temp = getXref(obs);
+	printf("%s", temp[0]);
+	printf("<</Size 4 /Root 1 0 R>>\n");
+	printf("startxref\n");
+	printf("%d\n", currentLength);
+	printf("%%%%EOF\n");
+
+	free(temp);
 
 	return 0;
 }

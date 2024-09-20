337e41866f544da56a7e63c99bed402c9932025e
ModEraNoah <klauck02@outlook.com>
2024-09-20 17:23:42 +0200

    mvoing object content creation into own function

--- main.c
+++ main.c
@@ -8,25 +8,70 @@ static uint objectCounter = 0;
 typedef struct {
 	uint objectNumber;
 	char *content;
-	uint length;
 } Object;
 
 char* getStart() {
 	return "%PDF-1.7\n";
 }
 
-Object getPdfRoot() {
-	Object temp = {++objectCounter,"<</Type /Catalog /Pages 2 0 R>>"};
+
+char** craftObjectContent(int objectNumber, char contentString[]) {
+	char **res = malloc(sizeof(char*));
+
+	int charCounter = 1;
+	while (objectNumber/(charCounter * 10)) {
+		charCounter++;
+	}
+
+	char objectBeginning[charCounter + 8];
+	snprintf(objectBeginning, sizeof(objectBeginning), "%i 0 obj\n", objectNumber);
+
+	char *objectEnding = "\nendobj";
+
+	int length = strlen(objectBeginning) + strlen(contentString) + strlen(objectEnding);
+	char *content= (char*) malloc((length + 8) * sizeof(char));
+	res[0] = content;
+
+	strcat(content, objectBeginning);
+	strcat(content, contentString);
+	strcat(content, objectEnding);
+
+	return res;
+}
+
+Object getPdfRoot(int objectNumber) {
+	/*
+	int charCounter = 1;
+	while (objectNumber/(charCounter * 10)) {
+		charCounter++;
+	}
+
+	char objectBeginning[charCounter + 8];
+	snprintf(objectBeginning, sizeof(objectBeginning), "%i 0 obj\n", objectNumber);
+
+	char *objectEnding = "\nendobj";
+	char *rootCatalog = "<</Type /Catalog /Pages 2 0 R>>";
+
+	int length = strlen(objectBeginning) + strlen(rootCatalog) + strlen(objectEnding);
+	char *content= (char*) malloc((length + 8) * sizeof(char));
+
+	strcat(content, objectBeginning);
+	strcat(content, rootCatalog);
+	strcat(content, objectEnding);
+	*/
+	char **content = craftObjectContent(objectNumber, "<</Type /Catalog /Pages 2 0 R>>");
+
+	Object temp = {objectNumber, *content};
 	return temp;
 }
 
-Object getPagesRoot() {
-	Object temp = {++objectCounter,"<</Type /Pages /Kids [3 0 R] /Count 1>>"};
+Object getPagesRoot(int objectNumber) {
+	Object temp = {objectNumber,"<</Type /Pages /Kids [3 0 R] /Count 1>>"};
 	return temp;
 }
 
 char** getXref(Object obs[]) {
-	char ** t = malloc (10 * sizeof(char*));
+	char ** t = malloc (1 * sizeof(char*));
 
 	char num[4];
 	snprintf(num, sizeof(num), "%d\n", objectCounter+1);
@@ -43,7 +88,6 @@ char** getXref(Object obs[]) {
 		else 
 			offset = 16 + currentLength;
 		char numb[11];
-		printf("\toffset: %d\n", offset);
 		snprintf(numb, sizeof(numb), "%010d", offset);
 		currentLength += offset;
 		currentLength += strlen(obs[i].content);
@@ -57,25 +101,26 @@ char** getXref(Object obs[]) {
 
 int main(void) {
 	Object obs[2];
-	obs[0] = getPdfRoot();
-	obs[1] = getPagesRoot();
+	obs[0] = getPdfRoot(++objectCounter);
+	obs[1] = getPagesRoot(++objectCounter);
 	printf("%s",getStart());
 
-	printf("%i 0 obj\n", obs[0].objectNumber);
 	printf("%s\n", obs[0].content);
-	printf("endobj\n");
+	free(obs[0].content);
 
-	printf("%i 0 obj\n", obs[1].objectNumber);
-	printf("%s\n", obs[1].content);
-	printf("endobj\n");
-
-	// printf("before calling getXref");
-	char **temp = getXref(obs);
-	printf("xref\n");
-	printf("%s", temp[0]);
-	printf("trailer\n");
-
-	free(temp);
+	// printf("%i 0 obj\n", obs[1].objectNumber);
+	// printf("%s\n", obs[1].content);
+	// printf("endobj\n");
+	// printf("%s\n", obs[1].content);
+	// free(obs[1].content);
+	//
+	// // printf("before calling getXref");
+	// char **temp = getXref(obs);
+	// printf("xref\n");
+	// printf("%s", temp[0]);
+	// printf("trailer\n");
+	//
+	// free(temp);
 
 	return 0;
 }
